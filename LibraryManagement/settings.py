89bee4010935b4650a50831ec784b4e8d33e1aa8
AmirMohebzadeh <amirmohebzadeh906@gmail.com>
2026-07-30 13:59:55 +0430

    Update settings.py

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,19 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-6iqb#rh%o_ebvii0f(29966+45ovs)#-w_kyz1dxy-n!i4xl2#"
-)
+SECRET_KEY = "django-insecure-6iqb#rh%o_ebvii0f(29966+45ovs)#-w_kyz1dxy-n!i4xl2#"
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = []
 
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-6iqb#rh%o_ebvii0f(29966+45ovs)#-w_kyz1dxy-n!i4xl2#"
-)
+
 
 
 
@@ -52,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGOUT_REDIRECT_URL = 'home'
